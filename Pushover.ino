34ac0871e635fd629419b08f5a42c8187da6797b
spambus <spambus@gmail.com>
2014-05-30 10:22:14 +0200

    Create Pushover.ino

---
+++ Pushover.ino
@@ -0,0 +1,72 @@
+/*
+
+   Pushover sketch by M.J. Meijer 2014
+
+   Send pushover.net messages from the arduino
+
+*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = {0xDE,0xAC,0xBF,0xEF,0xFE,0xAA};
+
+// Pushover settings
+char pushoversite[] = "api.pushover.net";
+char apitoken[] = "your30characterapitokenhere123";
+char userkey [] = "your30characteruserkeygoeshere";
+
+int length;
+
+EthernetClient client;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print(F("Starting ethernet..."));
+  if(!Ethernet.begin(mac)) Serial.println("failed");
+  else Serial.println(Ethernet.localIP());
+
+  delay(5000);
+  Serial.println("Ready");
+}
+
+void loop()
+{
+  pushover("OMG, Yes it works!!!",0);  
+  delay(60000); 
+}
+
+byte pushover(char *pushovermessage, int priority)
+{
+  String message = pushovermessage;
+
+  length = 93 + message.length();
+
+  if(client.connect(pushoversite,80))
+  {
+    client.println("POST /1/messages.json HTTP/1.1");
+    client.println("Host: api.pushover.net");
+    client.println("Connection: close\r\nContent-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.print(length);
+    client.println("\r\n");;
+    client.print("token=");
+    client.print(apitoken);
+    client.print("&user=");
+    client.print(userkey);
+    client.print("&message=");
+    client.print(message);
+    client.print("&priority=");
+    client.print(priority);
+    while(client.connected())  
+    {
+      while(client.available())
+      {
+        char ch = client.read();
+        Serial.write(ch);
+      }
+    }
+    client.stop();
+  }  
+}
